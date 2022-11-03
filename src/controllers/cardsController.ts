62bbfa00f69e7c0aff48419a3b0bf32e823eb7e3
Arthur Nepomuceno <arth.nepomuceno@gmail.com>
2022-11-03 15:10:53 -0300

    :sparkles: feat: CRUD - cards. CREATE new card - OK

--- src/controllers/cardsController.ts
+++ src/controllers/cardsController.ts
@@ -5,13 +5,32 @@ export async function createCard(req: Request, res: Response) {
     const headers = req.headers;
     const body = req.body;
     const { apikey } = headers;
-    const { employeeId, number, securityCode, type } = body;
+    const { employeeId, password, isVirtual, originalCardId, isBlocked ,type } = body;
 
     await cardServices.checkApiKey(apikey)
     await cardServices.checkEmployeeId(employeeId);
     await cardServices.checkCardType(type, employeeId);
-
+    
+    const cardNumber = await cardServices.setCardNumber();
     const cardholderName = await cardServices.setCardHolderName(employeeId);
+    const expirationDate = await cardServices.setExpirationDate();
+    const securityCode = await cardServices.setSecurityCode();
+    const hiddenSecurityCode = await cardServices.hideSecurityCode(`${securityCode}`);
 
-    return res.status(201).send('ok');
+    const newCard = {
+        number: cardNumber,
+        employeeId,
+        cardholderName,
+        securityCode: hiddenSecurityCode,
+        expirationDate,
+        password,
+        isVirtual,
+        originalCardId,
+        isBlocked,
+        type
+    }
+
+    await cardServices.insertCard(newCard);
+
+    return res.status(201).send('Created.');
 }
--- src/services/cardsServices.ts
+++ src/services/cardsServices.ts
@@ -1,11 +1,17 @@
 import * as cardsRepository from "../repositories/cardRepository";
 import * as employeeRepository from "../repositories/employeeRepository";
 import * as companyRepository from "../repositories/companyRepository";
+import { CardInsertData } from "../repositories/cardRepository";
+import { faker } from "@faker-js/faker";
+import dayjs from "dayjs";
+import Cryptr from "cryptr";
+import dotenv from "dotenv";
+dotenv.config();
 
-export async function checkApiKey(key: string | string[]){
+export async function checkApiKey(key: string | string[]) {
     const response = await companyRepository.findByApiKey(key);
 
-    if(!response) throw {
+    if (!response) throw {
         type: "invalid_api_key",
         message: "_this api key does not exist_"
     }
@@ -16,11 +22,11 @@ export async function checkApiKey(key: string | string[]){
 export async function checkEmployeeId(employeeId: number) {
     const response = await employeeRepository.findById(employeeId);
 
-    if(!response) throw {
+    if (!response) throw {
         type: "invalid_user",
         message: "_this employeeId does not exist_"
     }
-    
+
     return;
 }
 
@@ -29,12 +35,12 @@ export async function setCardHolderName(employeeId: number) {
 
     const { fullName } = response;
     const cardholderName = fullName.toUpperCase()
-                                .split(' ')
-                                .filter(e => e.length >= 3);
+        .split(' ')
+        .filter(e => e.length >= 3);
 
 
-    for(let i = 0; i < cardholderName.length; i++){
-        if(i !== 0 && i !== cardholderName.length - 1){
+    for (let i = 0; i < cardholderName.length; i++) {
+        if (i !== 0 && i !== cardholderName.length - 1) {
             cardholderName[i] = cardholderName[i][0];
         }
     }
@@ -45,7 +51,7 @@ export async function setCardHolderName(employeeId: number) {
 export async function checkCardType(type: string | any, employeeId: number) {
     const response = await cardsRepository.findByTypeAndEmployeeId(type, employeeId);
 
-    if(response) throw {
+    if (response) throw {
         type: "unavailable_card_type",
         message: "_you are already have a card registered with this type_"
     }
@@ -54,16 +60,29 @@ export async function checkCardType(type: string | any, employeeId: number) {
 }
 
 export async function setCardNumber() {
+    const n1 = faker.datatype.number({min: 1000, max: 9999});
+    const n2 = faker.datatype.number({min: 1000, max: 9999});
+    const n3 = faker.datatype.number({min: 1000, max: 9999});
+    const n4 = faker.datatype.number({min: 1000, max: 9999});
     
+    return `${n1}` + `${n2}` + `${n3}` + `${n4}`;
 }
 
-//get expiration date
-export async function setExpirationDate(){}
+export async function setExpirationDate() {
+    const date = dayjs().add(5, 'years').format('MM/YY');
+    return date;
+ }
 
-//get security code
 export async function setSecurityCode() {
-    
+    const cvc = faker.datatype.number({min: 100, max: 999});
+    return cvc;
 }
 
-//encrypt security code
-export async function hideSecurityCode(cvc: string){}
+export async function hideSecurityCode(cvc: string) {
+    const cryptr = new Cryptr(process.env.SECRET_KEY);
+    return cryptr.encrypt(cvc);
+ }
+
+export async function insertCard(object: CardInsertData) {
+    return await cardsRepository.insert(object);
+}
